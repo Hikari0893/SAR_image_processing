8e5347edb43cad0d02ff7c58167ad32a8fce6a3f
Tonix22 <emiliotonix@gmail.com>
2023-09-08 17:19:04 -0600

    snipped

--- test1.py
+++ test1.py
@@ -2,42 +2,85 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#lectura de los datos
+# Load SAR image
 picture = np.load('converted_image.npy')
 #plt.imshow(picture[:,:,1])
 #plt.show()
 
-# Reconstruir la imagen compleja a partir de sus partes real e imaginaria
+# Reconstruct the complex image from its real and imaginary parts
 complex_image = picture[:, :, 0] + 1j * picture[:, :, 1]
 del picture
 
+# Use the modulus (magnitude) of the complex image
+magnitude_image = complex_image/np.max(np.abs(complex_image))
+
+# Flatten the 2D array to a 1D array
+flattened_data = np.abs(complex_image).flatten()
+
+# Create a histogram for the entire data
+plt.hist(flattened_data, bins=1000, edgecolor='black') 
+plt.show()
+plt.figure()
+plt.imshow(np.abs(magnitude_image))
+plt.show()
 
 # Apply fft transform
-spectrum_az    = np.fft.fftshift(np.fft.fft(complex_image,axis=0))
-del complex_image
-"""
 #spectrum_range = np.fft.fftshift(np.fft.fft(complex_image,axis=1))
-images_cols = complex_image.shape[0]
-images_rows = complex_image.shape[1]
-del complex_image
+images_cols_az = complex_image.shape[0]
+images_rows_range = complex_image.shape[1]
+spectrum_az  = np.zeros_like(complex_image)
+
+spectrum_az0 = np.fft.fftshift(np.fft.fft(complex_image[:,:7000]))
+spf0 = np.copy(spectrum_az0)
+del spectrum_az0
+
+"""spectrum_az1 = np.fft.fftshift(np.fft.fft(complex_image[:,7001:14000]))
+spf1 = np.copy(spectrum_az1)
+del spectrum_az1
+
+spectrum_az2 = np.fft.fftshift(np.fft.fft(complex_image[:,14001:21000]))
+spf2 = np.copy(spectrum_az2)
+del spectrum_az2
+
+spectrum_az3 = np.fft.fftshift(np.fft.fft(complex_image[:,21001:28000]))
+spf3 = np.copy(spectrum_az3)
+del spectrum_az3
+
+spectrum_az4 = np.fft.fftshift(np.fft.fft(complex_image[:,28001:29828]))
+spf4 = np.copy(spectrum_az4)
+del spectrum_az4
+
+del complex_image"""
 
 
-# 1. Genera las ventanas de Hamming 1D
-hamming_az    = 1/(np.hamming(images_cols))     #azimuth
+"""# Generate 1D Hamming windows
+
+#-------------------Parameters---------------------------
+alpha = 6.00000023841857910e-1
+f_az = 2.76500000000000000e3
+f_range = 1.50000000000000000e8
+#---------------------------------------------------------
+
+hamming_windows = alpha -(1 - alpha)*np.cos(2*np.pi*f_az)"""
+hamming_az    = 1/(np.hamming(images_cols_az))     #azimuth
 #hamming_range = 1/(np.hamming(images_rows))  #range
 
-dehamming_az  = spectrum_az*hamming_az[:, np.newaxis]
+dehamming_az  = spf0*hamming_az[:, np.newaxis]
+
+plt.figure()
+plt.plot(np.real(dehamming_az))  # Usar colormap "gray" para imágenes SAR
+plt.axis('off')  # Ocultar los ejes
+plt.savefig('odehamming_az.png', bbox_inches='tight', pad_inches=0)
+plt.show()
 
 #dehamming_sr  = spectrum_range@hamming_range
-"""
 
 
-# Usar el módulo (magnitud) de la imagen compleja
-#magnitude_image = complex_image/np.max(np.abs(complex_image))
+
 #print(magnitude_image)
 
 # Create the heatmap
-plt.imshow(np.log(np.abs(spectrum_az)), cmap='viridis')  
+"""plt.imshow(np.log(np.abs(spectrum_az)), cmap='viridis')  
 # 'viridis' is just one of many available colormaps
 plt.colorbar()  # Add a colorbar to the plot to show the value-to-color mapping
 
@@ -50,40 +93,7 @@ plt.ylabel('Y-axis')
 plt.show()
 print("dummy")
 
-"""
-# Flatten the 2D array to a 1D array
-flattened_data = np.abs(complex_image).flatten()
-
-# Create a histogram for the entire data
-plt.hist(flattened_data, bins=1000, edgecolor='black') 
-plt.show()
-plt.figure()
-plt.imshow(np.abs(magnitude_image))
-plt.show()
-"""
-"""
-# 1. Preparación de la Imagen: Escalado a [0, 1]
-scaled_image = (magnitude_image - np.min(magnitude_image)) / (np.max(magnitude_image) - np.min(magnitude_image))
-#print("scaled_image")
-#print(scaled_image)
-
-# 2. Visualización y Guardado
-plt.figure()
-plt.imshow(scaled_image, cmap='gray')  # Usar colormap "gray" para imágenes SAR
-plt.axis('off')  # Ocultar los ejes
-plt.savefig('original_image.png', bbox_inches='tight', pad_inches=0)
-plt.show()
-"""
-
-#----------------------------------------------------------------------------------------------------------------------------
-# Reconstruir la versión compleja de la imagen a partir de sus partes real e imaginaria
-complex_image = picture[:10000, :10000, 0] + 1j * picture[:10000, :10000, 1]
-
-# Aplicar FFT 2D
-#fft_image = np.fft.fftshift(np.fft.fft2(complex_image))
-spectrum_az = np.fft.fftshift(np.fft.fft(complex_image,axis=0))
-spectrum_range = np.fft.fftshift(np.fft.fft(complex_image,axis=1))
-print(spectrum_az)
+plt.imshow(dehamming_az)
 
 
 #Unweighting
@@ -96,28 +106,29 @@ alpha = 6.00000023841857910e-1
 #--------------------------------------------------------------------------------------------------------
 
 # 1. Genera las ventanas de Hamming 1D
-hamming_az = 1/(np.hamming(complex_image.shape[0]))
-hamming_range = 1/(np.hamming(complex_image.shape[1]))
+#hamming_az = 1/(np.hamming(complex_image.shape[0]))
+#hamming_range = 1/(np.hamming(complex_image.shape[1]))
 
 # 2. Crea la ventana de Hamming 2D
-hamming_2D = np.outer(hamming_az, hamming_range)
+#hamming_2D = np.outer(hamming_az, hamming_range)
 
 # 3. Multiplica la imagen por la ventana de Hamming 2D
 #windowed_image = complex_image * hamming_2D
 
 
 #----------------------------------------------------------------------------------------------------------
-print(hamming_az)
+#print(hamming_az)
 #H_range = 1/(alpha -(1 - alpha)*np.cos(2*np.pi*f_range))
 #----------------------------------------------------
-Az = spectrum_az*hamming_az
-print(Az)
-Range = spectrum_range*hamming_range
-
-plt.figure()
-plt.imshow(np.imag(picture[:,:,1]),vmax=255,vmin=0)
-plt.savefig('Az.png')
-plt.show()
+#Az = spectrum_az*hamming_az
+#print(Az)
+#Range = spectrum_range*hamming_range
+"""
+#plt.figure()
+#plt.imshow(np.imag(picture[:,:,1]),vmax=255,vmin=0)
+#plt.savefig('Az.png')
+#plt.show()
+"""
 #----------------------------------------------------
 
 def generate_sublooks(spectrum, num_sublooks):
@@ -137,9 +148,9 @@ def generate_sublooks(spectrum, num_sublooks):
         sublooks.append(sublook_space)
     
     return sublooks
-
+    del spectrum
 num_sublooks = 2  # Definir el número de sublooks que desea
-sublooks = generate_sublooks(Az, num_sublooks)
+sublooks = generate_sublooks(dehamming_az, num_sublooks)
 plt.plot(np.real(hamming_az))
 plt.savefig('espectro_az.png')
 plt.show()
@@ -153,4 +164,4 @@ for i, ax in enumerate(axs):
 
 plt.tight_layout()
 plt.savefig('image.png')
-plt.show()
+plt.show()"""
